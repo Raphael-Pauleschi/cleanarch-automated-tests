df69fc0ac659103f8d9f435388a48941fa3a486b
Raphael-Pauleschi <Raphael.f.pauleschi@gmail.com>
2024-04-11 16:20:58 -0300

    correção

--- __tests__/application/TransferenciaServico.test.ts
+++ __tests__/application/TransferenciaServico.test.ts
@@ -1,26 +1,27 @@
-import {TransferenciaServico} from "../../src/application/TransferenciaServico";
-import {MemoriaContaRepositorio} from "../fake/MemoriaContaRepositorio";
-import {TransferenciaDTO} from "../../src/application/dto/TransferenciaDTO";
+import { TransferenciaServico } from "../../src/application/TransferenciaServico";
+import { MemoriaContaRepositorio } from "../fake/MemoriaContaRepositorio";
+import { TransferenciaDTO } from "../../src/application/dto/TransferenciaDTO";
 import { Conta } from "../../src/model/Conta";
 import { Repositorio } from "../../src/model/contract/Repositorio";
 
-describe("Transferencia Serviço",async () =>{
-    test("transferir com sucesso", async()=>{})
- const repositorio: Repositorio<string,Conta> = new MemoriaContaRepositorio;
- const contaOrigem: Conta = new Conta("123456",500);
- const contaDestino: Conta = new Conta("645321",500);
- repositorio.adicionar(contaOrigem);
- repositorio.adicionar(contaDestino);
+describe("Transferência Serviço", () =>{
 
- const transferenciaServico: TransferenciaServico = new TransferenciaServico(repositorio);
- const dto: TransferenciaDTO = new TransferenciaDTO("123456","645321",100.0);
+    test("transferir com sucesso", async () => {
+        const repositorio: Repositorio<string, Conta> = new MemoriaContaRepositorio();
+        const contaOrigem: Conta = new Conta("123456", 5000);
+        const contaDestino: Conta = new Conta("654321", 5000);
+        repositorio.adicionar(contaOrigem);
+        repositorio.adicionar(contaDestino);
 
- const recibo: string = await transferenciaServico.transferir(dto);
-const contaOrigemRepo = await repositorio.buscar("123456");
-const contaDestinoRepo = await repositorio.buscar("654321");
+        const transferenciaServico: TransferenciaServico = new TransferenciaServico(repositorio);
+        const dto: TransferenciaDTO = new TransferenciaDTO("123456", "654321", 100.0);
 
-    expect(contaOrigemRepo!.saldo).toBe(4900.0);
-    expect(contaDestinoRepo!.saldo).toBe(4900.0);
-    expect(recibo.length).toBe(6);
-
-})
+        const recibo: string = await transferenciaServico.transferir(dto);
+        
+        const contaOrigemRepo = await repositorio.buscar("123456");
+        const contaDestinoRepo = await repositorio.buscar("654321");
+        expect(contaOrigemRepo!.saldo).toBe(4900.0);
+        expect(contaDestinoRepo!.saldo).toBe(5100.0);
+        expect(recibo.length).toBe(6);
+    });
+});
